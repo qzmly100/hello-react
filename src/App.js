f3c115a6ce01d4ef500759c0ab280a2b51497378
qzmly100 <104551626@qq.com>
2018-08-25 21:49:11 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+               我是叶俊峰
+            </div>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
